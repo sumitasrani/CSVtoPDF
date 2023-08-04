afa94915dd7c9db628d9228c567cff020604f0ca
sumitasrani <hello@sumitasrani.com>
2023-08-05 02:41:26 +0530

    Multiple PDF pages as required

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(10,21,200,21)
-    for i in range(row["Pages"]):
+    for i in range(row["Pages"]-1):
         pdf.add_page()
 
 
